811209796fa05e0937d72c2e5826ac8b3ee5247b
Weiss1441 <nurymmuratov1441@gmail.com>
2026-02-04 13:41:37 +0500

    fix for login v2

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 app.get('/tasks', (req, res) => res.sendFile(path.join(__dirname, 'views', 'tasks.html')));
 app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login .html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 
 app.post('/contact', (req, res) => {
   res.send('Message received (demo).');
